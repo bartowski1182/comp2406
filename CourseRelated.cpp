9c013cf4798f86c6f2c9c5835ea11640bf4b37e3
noneabove1182 <ckealty1182@gmail.com>
2013-02-13 18:50:50 -0500

    Update CourseRelated.cpp
    Addint canSetCourseCode()

--- CourseRelated.cpp
+++ CourseRelated.cpp
@@ -29,3 +29,32 @@ string CourseRelated::print()
 	return courseCode;
 
 }
+
+bool CourseRelated::canSetCourseCode ()
+{
+	
+	if (courseCode.size() != 9){
+		return false;
+	}
+	
+	unsigned letters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+	unsigned numbers = "1234567890"";
+	
+	for(int i = 0; i < 3; i++){		
+		if (courseCode.find_first_of(letters, i) != i + 1){
+			return false;
+		}
+	}
+	if (courseCode.find_first_of(" ") != 5){
+		return false;
+	}
+	for(int i = 5; i < 8; i++){
+		if (courseCode.find_first_of(numbers, i) != i + 1){
+			return false;
+		}
+	}
+	return true;
+	
+	
+	
+}
